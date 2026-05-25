9d5364b86d4afba40fb986dfbbb047724225bc29
manoanotoavina33 <manoanotoavina33@gmail.com>
2026-05-25 07:33:54 +0200

    feat: add WiFi AP setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-#define LED 2
+const char* AP_SSID ="SmartCount";
+const char* AP_PASSWORD= "Fortico1234";
+void setup(){
+  Serial.begin(115200);
+  delay(1000);
 
-void setup() {
-  pinMode(LED, OUTPUT);
+  WiFi.mode(WIFI_AP);
+  bool ok = WiFi.softAP(AP_SSID, AP_PASSWORD);
+
+  if (ok){
+    Serial.println("AP démarré");
+    Serial.print("IP: ");
+    Serial.println(WiFi.softAPIP());
+  } else{
+    Serial.println("Echec");
+  }
 }
-
-void loop() {
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
+void loop(){
+  
 }
